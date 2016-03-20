9e6f75a901cdaecc8ac4e2a1cbb91014941d0fac
yapcheahshen <yapcheahshen@gmail.com>
2016-03-21 00:50:45 +0800

    find out non sequence paragraph number

--- chinese.js
+++ chinese.js
@@ -1,14 +1,24 @@
 // Chinese related code
 var lastnum=1;
 var split_paragraph=function(text,fn) {
-	var last=0;
+	var last=0,newnum=0,newlast;
 	var out=[],paragraph=[];
-	text.replace(/<!(\d+)>/g,function(m,m1,idx){
+	text.replace(/<!([0-9\-]+)>/g,function(m,m1,idx){
 		out.push(text.substring(last,idx));
 		last=idx+m.length;
-		if (parseInt(m1)<lastnum) lastnum=1;//reset on DN14, DN24
+		if (m1.indexOf("-")>-1) { //RANGE <!xxx-yyy>
+			newnum=parseInt(m1.split("-")[0]);  //DN10 457-458
+			newlast=parseInt(m1.split("-")[1]);
+		} else {
+			newlast=newnum=parseInt(m1);
+		}
+		
+		if (newnum<lastnum) lastnum=1;//reset on DN14, DN24
+		else if (newnum!=lastnum+1) {
+			console.log("wrong paragraph number",fn,lastnum, newnum)
+		}
 		paragraph.push(lastnum);
-		lastnum=parseInt(m1);
+		lastnum=newlast;
 	})
 	paragraph.push(lastnum);
 	out.push(lastnum+"\t"+text.substr(last));
